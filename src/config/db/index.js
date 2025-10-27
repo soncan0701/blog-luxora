f711b17a8ca4690188552cc53edf41317dc11d03
soncan0701 <sonthaingocnguyen1902@gmail.com>
2025-10-27 17:25:22 +0700

    Update database connection string

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/VciEdenPark');
+        await mongoose.connect('mongodb+srv://sonthaingocnguyen1902_db_user:dEdALgYwBYdqPQzH@cluster0.lyg567q.mongodb.net/?appName=Cluster0');
         console.log('Connect successfully');
     } catch (error) {
         console.log('Connect fail');
